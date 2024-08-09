a2fdb3d16bbe3faa50a15cd7a899f4a58b4fa96f
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-08-09 19:14:23 +0200

    Implement React context API

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -5,10 +5,7 @@ import { createPortal } from 'react-dom';
 // Project Imports
 import Cart from './Cart';
 
-const CartModal = forwardRef(function Modal(
-  { cartItems, onUpdateCartItemQuantity, title, actions },
-  ref
-) {
+const CartModal = forwardRef(function Modal({ title, actions }, ref) {
   const dialog = useRef();
 
   useImperativeHandle(ref, () => {
@@ -22,7 +19,7 @@ const CartModal = forwardRef(function Modal(
   return createPortal(
     <dialog id="modal" ref={dialog}>
       <h2>{title}</h2>
-      <Cart items={cartItems} onUpdateItemQuantity={onUpdateCartItemQuantity} />
+      <Cart />
       <form method="dialog" id="modal-actions">
         {actions}
       </form>
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,9 +1,18 @@
-export default function Shop({ children }) {
+// Project Imports
+import { DUMMY_PRODUCTS } from '../dummy-products';
+import Product from './Product';
+
+export default function Shop() {
+  const productsList = DUMMY_PRODUCTS.map((product) => (
+    <li key={product.id}>
+      <Product {...product} />
+    </li>
+  ));
   return (
     <section id="shop">
       <h2>Elegant Clothing For Everyone</h2>
 
-      <ul id="products">{children}</ul>
+      <ul id="products">{productsList}</ul>
     </section>
   );
 }
